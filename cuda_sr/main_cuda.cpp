403a7c1763e54bf75b7f3cd2e9c42571e92b899e
Jayden Bosch <104544560+ChiaSerYong@users.noreply.github.com>
2025-04-24 17:23:16 +0800

    Commit 4
    seam_carving_cuda.h

--- cuda_sr/main_cuda.cpp
+++ cuda_sr/main_cuda.cpp
@@ -1,5 +1,5 @@
 #include "data_structures.h"  // Include our new header file
-#include "seam_carving_cuda.cuh"
+#include "seam_carving_cuda.h"
 #include <chrono>
 #include <iostream>
 #include <string>
